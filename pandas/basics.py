4f7e99c2e92df0e62aae21e7ee224e94498ad885
OchiengHosea <jrhosea8@gmail.com>
2019-11-04 05:43:45 +0300

    Reading a csv file efficiently

--- pandas/basics.py
+++ pandas/basics.py
@@ -1,4 +1,23 @@
 import csv
+from pathlib import Path
+import datetime
 
+data_path = Path('pandas/amazon.csv')
 
-print("Importing csv")
+def clean_row(source_row):
+    source_row['state'] = source_row['state']
+    source_row['month'] = source_row['month']
+    source_row['year'] = int(source_row['year'])
+    source_row['number'] = float(source_row['number'])
+    source_row['date'] = datetime.datetime.strptime(source_row['date'], '%Y-%m-%d').date()
+    return source_row
+    
+
+def cleanse(reader):
+    for row in reader:
+        yield clean_row(row)
+
+with data_path.open() as data_file:
+    data_reader = csv.DictReader(data_file)
+    clean_data_reader = cleanse(data_reader)
+    print(list(clean_data_reader)[:5])
