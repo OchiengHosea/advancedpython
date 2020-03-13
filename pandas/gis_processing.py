4469d5ba6f153514d842f788cc19581347d2b550
Ochieng__ <jrhosea8@gmail.com>
2020-03-13 08:14:02 +0300

    Optimization basics

--- pandas/gis_processing.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import os
-
-pd.read_csv('pandas/gisdata.csv')
--- pandas/multiple_contexts_io.py
+++
@@ -1,5 +0,0 @@
-# Commonly used when converting a file data from one format to another
-# this needs to have two contexts 
-# one for reading the file and one for writting
-import re
-pattern_text = r'\[(?P<date>\d+-\d+-\d+ \d+:\d+:\d+, \d+)\]'
